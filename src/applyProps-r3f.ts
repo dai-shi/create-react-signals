2d4e78c0a475778fb978ba8b78a47d68af453a9c
daishi <daishi@axlight.com>
2023-01-13 01:26:52 +0900

    cache

--- src/applyProps-r3f.ts
+++ src/applyProps-r3f.ts
@@ -1,8 +1,15 @@
+let mod: any;
+
 export const applyProps = (
   instance: any,
   props: { [key: string]: unknown },
 ) => {
+  if (mod) {
+    mod.applyProps(instance, props);
+    return;
+  }
   import('@react-three/fiber').then((m) => {
+    mod = m;
     m.applyProps(instance, props);
   });
 };
