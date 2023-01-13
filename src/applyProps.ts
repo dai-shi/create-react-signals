16e9513ae4fc04a6420c2a02f2bfe451533f99be
Daishi Kato <dai-shi@users.noreply.github.com>
2023-01-13 21:57:14 +0900

    feat: apply props (#4)
    * feat: apply props
    
    * refactor
    
    * dynamic import
    
    * cache
    
    * export applyProps
    
    * refactor
    
    * patch package
    
    * patch-package in dep
    
    * no patch package
    
    * fix scripts
    
    * add missing var
    
    * add imaginary RN support
    
    * try webpackIgnore
    
    * Revert "try webpackIgnore"
    
    This reverts commit a099f2f6e62207d9438b2c88f09576fe235594d4.
    
    * use require instead of dynamic import
    
    * Revert "use require instead of dynamic import"
    
    This reverts commit f57d1e5b93c42d926738c6ee13afff0328c88fef.
    
    * update CHANGELOG

---
+++ src/applyProps.ts
@@ -0,0 +1,69 @@
+import { setValueForStyles } from './vendor/react-dom';
+
+export type Props = { [key: string]: unknown };
+
+export const applyPropsDOM = (instance: Element, props: Props) => {
+  Object.entries(props).forEach(([key, value]) => {
+    if (key === 'children') {
+      instance.textContent = value as string;
+    } else if (key === 'style') {
+      setValueForStyles(
+        instance,
+        Array.isArray(value) ? Object.assign({}, ...value) : (value as object),
+      );
+    } else {
+      (instance as any)[key] = value;
+    }
+  });
+};
+
+let r3fModule: any;
+
+const applyPropsR3F = (instance: any, props: Props) => {
+  if (!r3fModule) {
+    import('@react-three/fiber').then((m) => {
+      r3fModule = m;
+      applyPropsR3F(instance, props);
+    });
+    return;
+  }
+  r3fModule.applyProps(instance, props);
+};
+
+let rnModule: any;
+
+// FIXME untested
+// TODO support text instance
+const applyPropsRN = (instance: any, props: Props) => {
+  if (!rnModule) {
+    import(
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      'react-native/Libraries/ReactPrivate/ReactNativePrivateInterface'
+    ).then((m) => {
+      rnModule = m;
+      applyPropsRN(instance, props);
+    });
+    return;
+  }
+  const { UIManager } = rnModule;
+  UIManager.updateView(
+    instance._nativeTag,
+    instance.viewConfig.uiViewClassName,
+    props,
+  );
+};
+
+export const applyProps = (instance: any, props: Props) => {
+  let fn: typeof applyProps;
+  if (typeof Element !== 'undefined' && instance instanceof Element) {
+    fn = applyPropsDOM;
+  } else if (instance.__r3f) {
+    fn = applyPropsR3F;
+  } else if (instance._nativeTag && instance.viewConfig) {
+    fn = applyPropsRN;
+  } else {
+    throw new Error('Cannot detect renderer type');
+  }
+  fn(instance, props);
+};
---
+++ src/vendor/react-dom.js
@@ -0,0 +1,89 @@
+// copied from https://github.com/facebook/react/blob/0b4f443020af386f2b48c47c074cb504ed672dc8/packages/react-dom-bindings/src/client/CSSPropertyOperations.js#L59
+export function setValueForStyles(node, styles) {
+  const style = node.style;
+  for (let styleName in styles) {
+    if (!styles.hasOwnProperty(styleName)) {
+      continue;
+    }
+    const isCustomProperty = styleName.indexOf('--') === 0;
+    const styleValue = dangerousStyleValue(
+      styleName,
+      styles[styleName],
+      isCustomProperty,
+    );
+    if (styleName === 'float') {
+      styleName = 'cssFloat';
+    }
+    if (isCustomProperty) {
+      style.setProperty(styleName, styleValue);
+    } else {
+      style[styleName] = styleValue;
+    }
+  }
+}
+
+// copied from https://github.com/facebook/react/blob/0b4f443020af386f2b48c47c074cb504ed672dc8/packages/react-dom-bindings/src/shared/dangerousStyleValue.js#L20
+function dangerousStyleValue(name, value, isCustomProperty) {
+  const isEmpty = value == null || typeof value === 'boolean' || value === '';
+  if (isEmpty) {
+    return '';
+  }
+  if (
+    !isCustomProperty &&
+    typeof value === 'number' &&
+    value !== 0 &&
+    !(isUnitlessNumber.hasOwnProperty(name) && isUnitlessNumber[name])
+  ) {
+    return value + 'px'; // Presumes implicit 'px' suffix for unitless numbers
+  }
+  return ('' + value).trim();
+}
+
+// copied from https://github.com/facebook/react/blob/0b4f443020af386f2b48c47c074cb504ed672dc8/packages/react-dom-bindings/src/shared/CSSProperty.js#L11
+const isUnitlessNumber = {
+  animationIterationCount: true,
+  aspectRatio: true,
+  borderImageOutset: true,
+  borderImageSlice: true,
+  borderImageWidth: true,
+  boxFlex: true,
+  boxFlexGroup: true,
+  boxOrdinalGroup: true,
+  columnCount: true,
+  columns: true,
+  flex: true,
+  flexGrow: true,
+  flexPositive: true,
+  flexShrink: true,
+  flexNegative: true,
+  flexOrder: true,
+  gridArea: true,
+  gridRow: true,
+  gridRowEnd: true,
+  gridRowSpan: true,
+  gridRowStart: true,
+  gridColumn: true,
+  gridColumnEnd: true,
+  gridColumnSpan: true,
+  gridColumnStart: true,
+  fontWeight: true,
+  lineClamp: true,
+  lineHeight: true,
+  opacity: true,
+  order: true,
+  orphans: true,
+  tabSize: true,
+  widows: true,
+  zIndex: true,
+  zoom: true,
+
+  // SVG-related properties
+  fillOpacity: true,
+  floodOpacity: true,
+  stopOpacity: true,
+  strokeDasharray: true,
+  strokeDashoffset: true,
+  strokeMiterlimit: true,
+  strokeOpacity: true,
+  strokeWidth: true,
+};
