64409015e65a9bc6256b81ae16ed439be71fa2f4
daishi <daishi@axlight.com>
2023-01-13 15:05:27 +0900

    add imaginary RN support

--- src/applyProps.ts
+++ src/applyProps.ts
@@ -30,12 +30,38 @@ const applyPropsR3F = (instance: any, props: Props) => {
   r3fModule.applyProps(instance, props);
 };
 
+let rnModule: any;
+
+// FIXME untested
+// TODO support text instance
+const applyPropsRN = (instance: any, props: Props) => {
+  if (!rnModule) {
+    import(
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      'react-native/Libraries/ReactPrivate/ReactNativePrivateInterface'
+    ).then((m) => {
+      rnModule = m;
+      applyPropsRN(instance, props);
+    });
+    return;
+  }
+  const { UIManager } = rnModule;
+  UIManager.updateView(
+    instance._nativeTag,
+    instance.viewConfig.uiViewClassName,
+    props,
+  );
+};
+
 export const applyProps = (instance: any, props: Props) => {
   let fn: typeof applyProps;
   if (typeof Element !== 'undefined' && instance instanceof Element) {
     fn = applyPropsDOM;
   } else if (instance.__r3f) {
     fn = applyPropsR3F;
+  } else if (instance._nativeTag && instance.viewConfig) {
+    fn = applyPropsRN;
   } else {
     throw new Error('Cannot detect renderer type');
   }
